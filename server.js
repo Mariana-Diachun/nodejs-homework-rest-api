909adab8c1cf978ffe83f4e74542e585e51bff0d
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2023-01-10 12:31:11 +0100

    add started files, mongoose, env

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.set("strictQuery", false);
+
+const { HOST_URI } = process.env;
+
+async function main() {
+  try {
+    console.log(HOST_URI);
+    await mongoose.connect(HOST_URI);
+    console.log("Database connection successful");
+  } catch (error) {
+    console.error(error.message);
+  }
+  process.exit(1);
+}
+
+main();
